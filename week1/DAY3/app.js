6cb8e7937a8d299d95cf2b129d9593022ce010e8
Vivek SIngh <viveks.hestabit@gmail.com>
2025-12-14 22:58:27 +0530

    ALl files Push to remote repo

--- week1/DAY3/app.js
+++ week1/DAY3/app.js
@@ -1,7 +1,7 @@
 console.log('Hello world');
 console.log('commit 2');
 console.log('commit 3');
-console.log('commit 5'
+console.log('commit 5');
 console.log('Commit 6');
 console.log('Commit 7');
 console.log('Commit 8');
